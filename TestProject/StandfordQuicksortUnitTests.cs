3262cfeef48ad2989422b25735a9271d88e2ce2b
adamelhem <fm_post@hotmail.com>
2025-04-25 10:39:23 +0300

    Tests for QuickSort

--- TestProject/StandfordQuicksortUnitTests.cs
+++ TestProject/StandfordQuicksortUnitTests.cs
@@ -12,19 +12,27 @@ namespace TestProject
         }
 
         [Test]
-        public void TestQuickSort()
-        {
-            var input = new int[] { 12, 11, 13, 5, 6, 7 };
-            var sorted = _BL.QuickSort(input);
-            Assert.That(sorted, Is.EqualTo(new int[] { 5, 6, 7, 11, 12, 13 }));
-        }
-
-        [Test]
         public void TestSwap()
         {
             var input = new int[] { 1, 2 };
             var sorted = _BL.Swap(input, 0, 1);
-            Assert.That(sorted, Is.EqualTo(new int[] {2,1 }));
+            Assert.That(sorted, Is.EqualTo(new int[] { 2, 1 }));
+        }
+
+        [Test]
+        public void TestPartion()
+        {
+            var input = new int[] { 7, 6, 10, 5, 9, 2, 1, 15, 7 };
+            var sorted = _BL.Partion(input, 0, input.Length - 1);
+            Assert.That(sorted, Is.EqualTo(new int[]{ 2, 6, 1, 5, 7, 7, 10, 9, 15}));
+        }
+
+        [Test]
+        public void TestQuickSort()
+        {
+            var input = new int[] { 7, 6, 10, 5, 9, 2, 1, 15, 7 };
+            var sorted = _BL.QuickSort(input);
+            Assert.That(sorted, Is.EqualTo(new int[] { 1, 2, 5, 6, 7, 7, 9, 10, 15 }));
         }
 
         [Test]
