5466919d96264102cd09feb6a0e49d63d3a401ce
adamelhem <fm_post@hotmail.com>
2025-04-28 15:33:46 +0300

    StandfordQuicksortMedianOf3BL

---
+++ BusinessLogic/StandfordQuicksortMedianOf3BL.cs
@@ -0,0 +1,51 @@
+﻿
+
+namespace BusinessLogic
+{
+    public class StandfordQuicksortMedianOf3BL
+    {
+        private double _CompareCount;
+
+        public double GetCompareCount()
+        {
+            return _CompareCount;
+        }
+
+        public void QuickSort(double[] input, int l, int r)
+        {
+            if ( l >= r )
+            {
+                return;
+            }
+            _CompareCount = _CompareCount + r-l+1;
+            var p = Partion(input, l, r);
+            QuickSort(input, l, p - 1);
+            QuickSort(input, p, r);
+        }
+
+        public int Partion(double[] input, int l, int r)
+        {
+            var p = input[l];
+            var i = l+1;
+            for (var j = l+1 ; j <= r; j++)
+            {
+                if (input[j] < p)
+                {
+                    Swap(input, j, i);
+                    i++;
+                }
+            }
+            Swap(input, l, i - 1);
+            return i;
+        }
+
+        public double[] Swap(double[] input, int p1, int p2)
+        {
+            var temp = input[p1];
+            input[p1] = input[p2];
+            input[p2] = temp;
+            return input;
+        }
+
+    }
+}
