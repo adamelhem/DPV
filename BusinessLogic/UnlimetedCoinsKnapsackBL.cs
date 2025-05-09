ea21a3932f21c5af024b2e18a4d2b8db36c89832
adamelhem <fm_post@hotmail.com>
2025-05-09 15:24:18 +0300

    UnlimetedCoinsKnapsackBL

---
+++ BusinessLogic/UnlimetedCoinsKnapsackBL.cs
@@ -0,0 +1,26 @@
+﻿
+namespace BusinessLogic
+{
+    public class UnlimetedCoinsKnapsackBL
+    {
+        public bool CanMakeChange(int n, int v, int[] x)
+        {
+            int[] T = new int[v + 1];
+
+            T[0] = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 1; j < v; j++)
+                {
+                    if (x[i] <= j)
+                    {
+                        T[j] = Math.Max(T[j], T[j - x[i]] + x[i]);
+                    }
+                }
+            }
+            return (T[v] == v);
+        }
+    }
+}
+ 
---
+++ TestProject/UnlimetedCoinsBLunitTests.cs
@@ -0,0 +1,39 @@
+﻿using BusinessLogic;
+
+namespace TestProject
+{
+    public class UnlimetedCoinsBLunitTests
+    {
+        private UnlimetedCoinsKnapsackBL _BL;
+
+        [SetUp]
+        public void Setup()
+        {
+            _BL = new UnlimetedCoinsKnapsackBL();
+        }
+
+        [Test]
+        public void Test1()
+        {
+
+            /*
+             * Input: sum = 10, coins[] = [2, 5, 3, 6]
+             * Output: 5
+             * Explanation: 
+             * There are five solutions: 
+             * [2, 2, 2, 2, 2], 
+             * [2, 2, 3, 3], 
+             * [2, 2, 6], 
+             * [2, 3, 5],
+             * [5, 5]                                          
+             */
+
+            int n = 4/* your value for n */;
+            int v = 10/* your value for v */;
+            int[] x = { 2, 5, 3, 6 }; // Initialize your array x with appropriate values
+            var result = _BL.CanMakeChange(n, v, x);
+            Assert.That(result, Is.True);
+        }
+
+    }
+}
