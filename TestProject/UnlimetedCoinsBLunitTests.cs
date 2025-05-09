094c4841f05f6df4d441669876271f3c5078e5b0
adamelhem <fm_post@hotmail.com>
2025-05-09 15:50:07 +0300

    more tests

--- TestProject/UnlimetedCoinsBLunitTests.cs
+++ TestProject/UnlimetedCoinsBLunitTests.cs
@@ -30,8 +30,53 @@ namespace TestProject
 
             int v = 10/* your value for v */;
             int[] x = { 2, 5, 3, 6 }; // Initialize your array x with appropriate values
-            var result = _BL.CanMakeChange( v, x);
+            var result = _BL.CanMakeChange(v, x);
             Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            int v = 5/* your value for v */;
+            int[] x = { 4 }; // Initialize your array x with appropriate values
+            var result = _BL.CanMakeChange(v, x);
+            Assert.That(result, Is.False);
+        }
+
+        [Test]
+        public void Test3()
+        {
+            int v = 30/* your value for v */;
+            int[] x = { 25, 10, 5 }; // Initialize your array x with appropriate values
+            var result = _BL.CanMakeChange(v, x);
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void Test4()
+        {
+            int v = 19/* your value for v */;
+            int[] x = { 9, 6, 5, 1 }; // Initialize your array x with appropriate values
+            var result = _BL.CanMakeChange(v, x);
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void Test5()
+        {
+            int v = 0/* your value for v */;
+            int[] x = { 1, 5 }; // Initialize your array x with appropriate values
+            var result = _BL.CanMakeChange(v, x);
+            Assert.That(result, Is.True);
+        }
+
+        [Test]
+        public void Test6()
+        {
+            int v = 5/* your value for v */;
+            int[] x = { 4, 6, 2 }; // Initialize your array x with appropriate values
+            var result = _BL.CanMakeChange(v, x);
+            Assert.That(result, Is.False);
         }
 
     }
